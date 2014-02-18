4cf77a2f1f7c68dac79a97f124e04f487ee083f8
Jake Vanderplas <vanderplas@astro.washington.edu>
2014-02-18 14:45:45 -0800

    reduce data duplication

--- mpld3renderer.py
+++ mpld3renderer.py
@@ -16,22 +16,60 @@ class MPLD3Renderer(Renderer):
         self.finished_figures = []
 
     def add_data(self, data):
-        if not self.figure_json:
-            raise ValueError("Cannon add data when no figure is open")
-        datalabel = "data{0:2d}".format(len(self.figure_json['data']) + 1)
-        self.figure_json['data'][datalabel] = np.asarray(data).tolist()
-        return datalabel
+        # Check if any column of the data exists elsewhere
+        # If so, we'll use that dataset rather than duplicating it.
+        data = np.asarray(data)
+        if data.ndim != 2 and data.shape[1] != 2:
+            raise ValueError("Data is expected to be of size [N, 2]")
+
+        # here we check whether any previously-added dataset
+        # is a potential match
+        matching_data = -1
+        for i, d in enumerate(self.datasets):
+            if data.shape[0] != d.shape[0]:
+                continue
+
+            matches = np.array([np.all(col == d.T, axis=1)
+                                for col in data.T])
+            if np.any(matches):
+                matching_data = i
+                break
+
+        if matching_data >= 0:
+            new_data = list(self.datasets[matching_data].T)
+            indices = []
+            for i in range(data.shape[1]):
+                match = np.where(matches[i])[0]
+                if len(match):
+                    indices.append(match[0])
+                else:
+                    new_data.append(data[:, i])
+                    indices.append(len(new_data) - 1)
+
+            self.datasets[matching_data] = np.asarray(new_data).T
+            datalabel = self.datalabels[matching_data]
+            xindex, yindex = map(int, indices)
+        else:
+            self.datasets.append(data)
+            datalabel = "data{0:02d}".format(len(self.figure_json['data']) + 1)
+            xindex = 0
+            yindex = 1
+
+        self.datalabels.append(datalabel)
+        return {"data":datalabel, "xindex":xindex, "yindex":yindex}
 
     def open_figure(self, fig, props):
+        self.datasets = []
+        self.datalabels = []
         self.figure_json = dict(width=props['figwidth'] * props['dpi'],
                              height=props['figheight'] * props['dpi'],
                              axes=[],
                              data={})
 
     def close_figure(self, fig):
-        if not hasattr(self, "finished_figs"):
-            self.finished_figs = []
-        self.finished_figs.append((fig, self.figure_json))
+        for datalabel, dataset in zip(self.datalabels, self.datasets):
+            self.figure_json['data'][datalabel] = np.asarray(dataset).tolist()
+        self.finished_figures.append((fig, self.figure_json))
 
     def open_axes(self, ax, props):
         self.axes_json = dict(bbox=props['bounds'],
@@ -48,13 +86,13 @@ class MPLD3Renderer(Renderer):
         self.axes_json = None
 
     def draw_line(self, data, coordinates, style):
-        line = dict(data=self.add_data(data))
+        line = self.add_data(data)
         for key in ['color', 'linewidth', 'dasharray', 'alpha']:
             line[key] = style[key]
         self.axes_json['lines'].append(line)
 
     def draw_markers(self, data, coordinates, style):
-        markers = dict(data=self.add_data(data))
+        markers = self.add_data(data)
         for key in ['facecolor', 'edgecolor', 'edgewidth', 'alpha']:
             markers[key] = style[key]
         self.axes_json['markers'].append(markers)
@@ -76,7 +114,7 @@ def fig_to_d3(fig, mpld3_loc="js/mpld3.v1.js",
     figid = str(int(random.random() * 1E11))
     renderer = MPLD3Renderer()
     Exporter(renderer).run(fig)
-    figure_json = json.dumps(renderer.finished_figs[0][1])
+    figure_json = json.dumps(renderer.finished_figures[0][1])
     return MPLD3_TEMPLATE.render(figid=figid,
                                  d3_loc=d3_loc,
                                  mpld3_loc=mpld3_loc,
