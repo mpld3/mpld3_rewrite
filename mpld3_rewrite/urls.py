8bfe3ba3621e48628f9bb04544aaad8b8379f82e
Jake Vanderplas <vanderplas@astro.washington.edu>
2014-03-01 06:51:39 -0800

    fix urls

--- mpld3_rewrite/urls.py
+++ mpld3_rewrite/urls.py
@@ -6,7 +6,7 @@ import warnings
 warnings.warn("using temporary MPLD3_URL: switch to ghpages ASAP!")
 
 D3_URL = "http://d3js.org/d3.v3.min.js"
-MPLD3_URL = "http://rawgithub.com/mpld3/mpld3_rewrite/master/js/mpld3.v0.1.js"
+MPLD3_URL = "https://rawgithub.com/mpld3/mpld3_rewrite/master/mpld3_rewrite/js/mpld3.v0.1.js"
 
 D3_LOCAL = os.path.join(__path__[0], "js", "d3.v3.min.js")
 MPLD3_LOCAL = os.path.join(__path__[0], "js", "mpld3.v0.1.js")
